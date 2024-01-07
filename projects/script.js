ba59cda945f49f9ce039e255e00c46d0f855260b
Amritanshu02 <78687085+Amritanshu02@users.noreply.github.com>
2024-01-07 15:23:43 +0530

    Uploaded code

--- projects/script.js
+++ projects/script.js
@@ -1,33 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#menu').click(function(){
+    $('#menu').click(function () {
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
 
-    $(window).on('scroll load',function(){
+    $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
 
-        if(window.scrollY>60){
+        if (window.scrollY > 60) {
             document.querySelector('#scroll-top').classList.add('active');
-        }else{
+        } else {
             document.querySelector('#scroll-top').classList.remove('active');
         }
     });
 });
 
 document.addEventListener('visibilitychange',
-function(){
-    if(document.visibilityState === "visible"){
-        document.title = "Projects | Portfolio Saurav Mukherjee";
-        $("#favicon").attr("href","/assests/images/favicon.png");
-    }
-    else {
-        document.title = "Come Back To Portfolio";
-        
-    }
-});
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio Amritanshu";
+            $("#favicon").attr("href", "/assests/images/portrait.jpeg");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+
+        }
+    });
 
 
 // fetch projects start
@@ -63,22 +63,22 @@ function showProjects(projects) {
     });
     projectsContainer.innerHTML = projectsHTML;
 
-// vanilla tilt.js
-VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    max: 20,
-});
-// vanilla tilt.js  
+    // vanilla tilt.js
+    VanillaTilt.init(document.querySelectorAll(".tilt"), {
+        max: 20,
+    });
+    // vanilla tilt.js  
 
-/* ===== SCROLL REVEAL ANIMATION ===== */
-const srtop = ScrollReveal({
-    origin: 'bottom',
-    distance: '80px',
-    duration: 1000,
-    reset: true
-});
+    /* ===== SCROLL REVEAL ANIMATION ===== */
+    const srtop = ScrollReveal({
+        origin: 'bottom',
+        distance: '80px',
+        duration: 1000,
+        reset: true
+    });
 
-/* SCROLL PROJECTS */
-srtop.reveal('.work .box',{interval: 200}); 
+    /* SCROLL PROJECTS */
+    srtop.reveal('.work .box', { interval: 200 });
 }
 
 getProjects().then(data => {
@@ -87,32 +87,32 @@ getProjects().then(data => {
 // fetch projects end
 
 // Start of Tawk.to Live Chat
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-  (function(){
-  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-  s1.async=true;
-  s1.src='https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
-  s1.charset='UTF-8';
-  s1.setAttribute('crossorigin','*');
-  s0.parentNode.insertBefore(s1,s0);
-  })();
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
 // End of Tawk.to Live Chat
 
 // disable developer mode
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
 }
